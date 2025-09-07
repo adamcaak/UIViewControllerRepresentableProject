4d81cfada12618109dfcbb3296f07f79b5da5ec2
adamcaak <ladamczak1997@gmail.com>
2025-09-07 16:35:36 +0200

    Initial Commit

---
+++ UIViewControllerRepresentableProject/UIViewControllerRepresentableProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresentableProjectApp.swift
+//  UIViewControllerRepresentableProject
+//
+//  Created by Łukasz Adamczak on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresentableProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
